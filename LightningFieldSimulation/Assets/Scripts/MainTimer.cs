06bfb62040aa87b2a6ba7e7d4a5dd9c80ecedb41
RobertGaines <rngaines@ucsc.edu>
2018-06-12 16:40:58 +0900

    Lightning now spawns and fades away

---
+++ LightningFieldSimulation/Assets/Scripts/MainTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTimer : MonoBehaviour {
+
+	public float strikeRange = 500f;
+	public Transform bolt;
+	
+	// Use this for initialization
+	void Start() {
+		StartCoroutine(StrikeTimer());
+	}
+
+	IEnumerator StrikeTimer() {
+		yield return new WaitForSeconds(0);
+		while (true) {
+			Strike();
+			yield return new WaitForSeconds(Random.Range(0.0f, 1.0f));
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void Strike() {
+		print ("\tI'm striking");
+
+		Vector3 pos = new Vector3(Random.Range(-strikeRange, strikeRange), 80f, Random.Range(-strikeRange, strikeRange));
+		/*GameObject newBolt =*/ Instantiate(bolt, pos, Quaternion.identity);//(GameObject)Instantiate(Resources.Load("Strike"));
+		//newBolt.transform.position = pos;
+	}
+}
---
+++ LightningFieldSimulation/Assets/Scripts/OnStrike.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStrike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//gameObject = what this script is attached to
+		if (gameObject.GetComponent<Renderer>().material.color.a <= 0) {
+			Destroy(gameObject);
+		}
+		gameObject.GetComponent<Renderer>().material.color -= new Color(0, 0, 0, 0.01f);
+	}
+}
